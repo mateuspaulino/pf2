95a07b5c7e30774e2829bb3f8d53158e831d442e
Mateus Paulino <contato@mateuspaulino.com.br>
2018-05-07 17:15:20 -0300

    requisicao

--- projeto_front/assets/js/App.js
+++ projeto_front/assets/js/App.js
@@ -5,16 +5,24 @@ app.controller("indexController", function($scope, $http){
 
     $scope.nome = "Mateus";
 
+    $scope.posts = [];
 
-    $http({
-        method: 'GET',
-        url: 'https://jsonplaceholder.typicode.com/posts/'
-      }).then(function successCallback(response) {
-          console.log(response.data);
-        console.log(response.status);
+    $scope.carregarPosts = function(){
+        $http({
+            method: 'GET',
+            url: 'https://jsonplaceholder.typicode.com/posts/'
+        }).then(function successCallback(response) {
+            console.log(response.data);
+            console.log(response.status);
+            $scope.posts = response.data;
         }, function errorCallback(response) {
             console.log(response.data);
             console.log(response.status);
         });
+    }
+
+    $scope.carregarPosts();
+
+   
 
 })
